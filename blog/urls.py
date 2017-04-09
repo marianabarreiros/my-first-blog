0d388d4c8cb8e037044c7d00de6b9950a2801d17
Mariana Barreiros <marianabarreiros.ti@gmail.com>
2017-04-09 20:21:34 -0300

    Added views to create/edit blog post inside the site.[3]

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    #adicionei o 'name='post-detail' na primeira url pois, quando eu tentava adicionar uma postagem nova dava erro: Reverse for 'post_detail' with arguments '()' and keyword arguments '{'pk': 13}' not found. 0 pattern(s) tried: []
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
